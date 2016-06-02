88be8cd3fd07e0dc0e8850f3c11d0829356d4629
ppeccin <ppeccin@gmail.com>
2016-06-02 11:59:20 -0300

    Kanji Font stated working

--- src/main/msx/slots/cartridges/special/kanji/CartridgeKanjiFont.js
+++ src/main/msx/slots/cartridges/special/kanji/CartridgeKanjiFont.js
@@ -12,13 +12,20 @@ wmsx.CartridgeKanjiFont = function(rom) {
         self.bytes = bytes;
         for(var i = 0, len = content.length; i < len; i++)
             bytes[i] = content[i];
+        jis2 = bytes.length === 262144;
     }
 
     this.connect = function(machine) {
+        // JIS 1
         machine.bus.connectInputDevice( 0xd8, wmsx.DeviceMissing.inputPortIgnored);
         machine.bus.connectInputDevice( 0xd9, this.inputD9);
         machine.bus.connectOutputDevice(0xd8, this.outputD8);
         machine.bus.connectOutputDevice(0xd9, this.outputD9);
+        // JIS 2
+        machine.bus.connectInputDevice( 0xda, wmsx.DeviceMissing.inputPortIgnored);
+        machine.bus.connectInputDevice( 0xdb, this.inputDB);
+        machine.bus.connectOutputDevice(0xda, this.outputDA);
+        machine.bus.connectOutputDevice(0xdb, this.outputDB);
     };
 
     this.disconnect = function(machine) {
@@ -26,6 +33,10 @@ wmsx.CartridgeKanjiFont = function(rom) {
         machine.bus.disconnectInputDevice( 0xd9, this.inputD9);
         machine.bus.disconnectOutputDevice(0xd8, this.outputD8);
         machine.bus.disconnectOutputDevice(0xd9, this.outputD9);
+        machine.bus.disconnectInputDevice( 0xda, wmsx.DeviceMissing.inputPortIgnored);
+        machine.bus.disconnectInputDevice( 0xdb, this.inputDB);
+        machine.bus.disconnectOutputDevice(0xda, this.outputDA);
+        machine.bus.disconnectOutputDevice(0xdb, this.outputDB);
     };
 
     this.powerOn = function() {
@@ -36,27 +47,46 @@ wmsx.CartridgeKanjiFont = function(rom) {
     };
 
     this.reset = function() {
-        charToRead = 0;
-        readAddress = 0;
+        charToRead1 = charToRead2 = 0;
+        readAddress1 = readAddress2 = 0;
     };
 
     this.outputD8 = function (val) {
-        charToRead = (charToRead & 0xfc0) | (val & 0x3f);
-        readAddress = charToRead << 5;
+        charToRead1 = (charToRead1 & 0xfc0) | (val & 0x3f);
+        readAddress1 = charToRead1 << 5;
     };
 
     this.outputD9 = function (val) {
-        charToRead = (charToRead & 0x03f) | ((val & 0x3f) << 6);
-        readAddress = charToRead << 5;
+        charToRead1 = (charToRead1 & 0x03f) | ((val & 0x3f) << 6);
+        readAddress1 = charToRead1 << 5;
+
+        //console.log("Set 1: " + charToRead1);
     };
 
     this.inputD9 = function () {
-        return bytes[readAddress++];
+        return bytes[readAddress1++ & 0x1ffff];
+    };
+
+    this.outputDA = function (val) {
+        charToRead2 = (charToRead2 & 0xfc0) | (val & 0x3f);
+        readAddress2 = charToRead2 << 5;
+    };
+
+    this.outputDB = function (val) {
+        charToRead2 = (charToRead2 & 0x03f) | ((val & 0x3f) << 6);
+        readAddress2 = charToRead2 << 5;
+
+        //console.log("Set 2: " + charToRead2);
+    };
+
+    this.inputDB = function () {
+        return jis2 ? bytes[0x20000 + (readAddress2++ & 0x1ffff)] : 0xff;
     };
 
 
-    var charToRead;
-    var readAddress;
+    var charToRead1, charToRead2;
+    var readAddress1, readAddress2;
+    var jis2 = false;
 
     var bytes;
     this.bytes = null;
@@ -72,6 +102,9 @@ wmsx.CartridgeKanjiFont = function(rom) {
             f: this.format.name,
             r: this.rom.saveState(),
             b: wmsx.Util.compressInt8BitArrayToStringBase64(bytes),
+            c1: charToRead1, r1: readAddress1,
+            c2: charToRead2, r2: readAddress2,
+            j2: jis2
         };
     };
 
@@ -79,16 +112,13 @@ wmsx.CartridgeKanjiFont = function(rom) {
         this.rom = wmsx.ROM.loadState(s.r);
         bytes = wmsx.Util.uncompressStringBase64ToInt8BitArray(s.b, bytes);
         this.bytes = bytes;
+        charToRead1 = s.c1; readAddress1 = s.r1;
+        charToRead2 = s.c2; readAddress2 = s.r2;
+        jis2 = s.j2;
     };
 
 
     if (rom) init(this);
-
-
-    this.eval = function(arg) {
-        return eval(arg);
-    }
-
 
 };
 
