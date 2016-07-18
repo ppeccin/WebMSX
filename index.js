ab5cf5785fe51bfd804479a89658bf62684297e7
ppeccin <ppeccin@gmail.com>
2016-07-18 20:03:36 -0300

    First release test

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/static'));
 
-app.get('/proxy-remote-download?url=:remoteurl', function(req, res) {
+app.get('/proxy-remote-download/', function(req, res) {
 
-    var url = req.params.remoteurl;
+    var url = req.params.u;
     console.log(">>> Serving proxy download from: " + url);
+
+    var url2 = req.u;
+    console.log(">>> Serving proxy download from: " + url2);
 
     request(url).pipe(res);
 
