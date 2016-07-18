6d80143676d4935b34c6d086cf966a8c10db9221
ppeccin <ppeccin@gmail.com>
2016-07-18 16:04:51 -0300

    Cleaning

---
+++ index.js
@@ -0,0 +1,25 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+//app.get('/', function(request, response) {
+//    response.render('pages/index');
+//});
+
+app.get('/', function(request, response) {
+    response.send("TEST WebMSX");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
