5db5509f59ca112c4a192b7f9a2bd174368084a8
ppeccin <ppeccin@gmail.com>
2016-07-18 22:18:43 -0300

    Testing

--- proxy-downloader/ProxyDownloader.js
+++ proxy-downloader/ProxyDownloader.js
@@ -13,7 +13,18 @@ function processGet(req, res) {
     var url = req.query.url;
     console.log(">>> Serving proxy download from: " + url);
 
+    // Return a good filename
     res.attachment(url);
+
+    // Restrict use
+
+    var cors = process.env.CORS_FROM;
+    if (cors) {
+        console.log(">>> CORS: " + cors);
+        res.header("Access-Control-Allow-Origin", cors);
+    }
+
+    // Fire
     request(url).pipe(res);
 
 }
