61e14c3d81027e43cc64c09744f00e12e1ba6544
ppeccin <ppeccin@gmail.com>
2016-11-29 23:44:33 -0200

    Alpha 4.0.27

--- src/main/Launcher.js
+++ src/main/Launcher.js
@@ -102,7 +102,7 @@ if (window.applicationCache) {
     else window.applicationCache.addEventListener("updateready", onUpdateReady);
 }
 
-WMSX.VERSION = "4.0.26";
+WMSX.VERSION = "4.0.27";
 
 // Start pre-loading images right away
 WMSX.preLoadImagesAndStart();
