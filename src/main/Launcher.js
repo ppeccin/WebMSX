be417b7f09a7bf367c6b3d27c80e63ea6764e606
ppeccin <ppeccin@gmail.com>
2016-12-08 18:21:25 -0200

    Gardening. Alpha 4.0.36

--- src/main/Launcher.js
+++ src/main/Launcher.js
@@ -103,7 +103,7 @@ if (window.applicationCache) {
     else window.applicationCache.addEventListener("updateready", onUpdateReady);
 }
 
-WMSX.VERSION = "4.0.35";
+WMSX.VERSION = "4.0.36";
 
 // Start pre-loading images right away
 WMSX.preLoadImagesAndStart();
