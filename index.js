548f9aacfd3ee27193c88871d72a66719e4ba307
ppeccin <ppeccin@gmail.com>
2017-12-27 14:12:46 -0200

    WS Test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 var express =  require('express');
-var proxy =    require('./proxy-downloader/ProxyDownloader');
+// var proxy =    require('./proxy-downloader/ProxyDownloader');
 var wsserver = require('./ws-server/WSServer');
 var port = process.env.PORT || 5000;
 
@@ -11,7 +11,7 @@ var port = process.env.PORT || 5000;
 var server = express();
 server.set('port', port);
 //server.use(express.static(__dirname + '/website'));
-server.get('/proxy-remote-download', proxy.processGet);
+// server.get('/proxy-remote-download', proxy.processGet);
 
 server.listen(server.get('port'), function() {
     console.log('WebMSX started on port', server.get('port'));
