02dc395545a92b1a4cf53ccca42514776168b594
ppeccin <ppeccin@gmail.com>
2018-01-04 17:57:37 -0200

    Port bugfix

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ require("./sessions/SessionManager");
 
 const express =  require("express");
 const proxy =    require("./proxy-downloader/ProxyDownloader");
-const port =     process.env.PORT || 80;
-
-console.log("env.PORT: " + process.env.PORT);
+const port =     process.env.PORT || 8081;
 
 
 // Proxy Downloader
