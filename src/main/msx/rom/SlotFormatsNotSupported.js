7f4c7ae66b8a1d472548363ac51cbdc06549068f
ppeccin <ppeccin@gmail.com>
2018-03-20 23:20:05 -0300

    ROM Database updated to version 20180320

--- src/main/msx/rom/SlotFormatsNotSupported.js
+++ src/main/msx/rom/SlotFormatsNotSupported.js
@@ -4,18 +4,19 @@
 
 wmsx.SlotFormatsNotSupported = {
 
-    "Arc": 0,               // Make Support?                                  1 game, no alts.           Copy protection Cart? Can't find ROM
-    "MatraInk": 0,          // Make Support?                                  1 game, no alts.           Special flash chip. Not so great game
-    "keyboardmaster": 0,    // Make Support?                                  1 game, has alts.          Special sound chip. Not released
-    "NettouYakyuu": 0,      // Make Support?                                  1 game, no alts.           Special sound chip. Can't find ROM
+    "Arc": 0,                       // Make Support?                                  1 game, no alts.           Copy protection Cart? Can't find ROM
+    "MatraInk": 0,                  // Make Support?                                  1 game, no alts.           Special flash chip. Not so great game
+    "KeyboardMaster": 0,            // Make Support?                                  1 game, has alts.          Special sound chip. Not released
+    "NettouYakyuu": 0,              // Make Support?                                  1 game, no alts.           Special sound chip. Can't find ROM
 
-    "Manbow2_2": 0,         // Points to Manbow2                              1 game, has alts.          Can't find ROM. Same mapper as Manbow2? (MegaFlash SCC)
-    "HamarajaNight": 0,     // Points to Manbow2                              1 game, has disk alts.     Can't find ROM. Same mapper as Manbow2? (MegaFlash SCC)
-    "Playball": 0,          // Resolves to Normal, works with no samples      2 games, no alts.          Special sound chip. Not so great games
+    "SuperPierrot": 0,              // Make Support?                                  1 game, no alts.
+    "KonamiUltimateCollection": 0,  // Make Support?                                  1 cart                     Special flash cart with built-in utils
+    "MSXAUDIO": 0,                  // Make Support?                                  System ROM
+    "MegaRAM": 0,                   // Make Support?
 
-    "MSXAUDIO": 0,          // Make Support                                   System ROM
-    "Kanji12": 0,           // Points to Kanji1, which supports both          System ROM
-
-    "MegaRAM": 0            // Make Support?
+    "Manbow2_2": 0,                 // Points to Manbow2                              1 game, has alts.          Can't find ROM. Same mapper as Manbow2? (MegaFlash SCC)
+    "HamarajaNight": 0,             // Points to Manbow2                              1 game, has disk alts.     Can't find ROM. Same mapper as Manbow2? (MegaFlash SCC)
+    "Playball": 0,                  // Resolves to Normal, works with no samples      2 games, no alts.          Special sound chip. Not so great games
+    "Kanji12": 0                    // Points to Kanji1, which supports both          System ROM
 
 };
