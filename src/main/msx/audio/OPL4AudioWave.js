ebe4c3e4dbacda6679b5893bd8975912e15d5213
ppeccin <ppeccin@gmail.com>
2018-07-19 23:42:00 -0300

    WIP MoonSound/OPL4

--- src/main/msx/audio/OPL4AudioWave.js
+++ src/main/msx/audio/OPL4AudioWave.js
@@ -377,25 +377,25 @@ wmsx.OPL4AudioWave = function(opl4) {
                 envStepLevelDur[cha] = d1r[cha] === 0 ? 0 : rateDecayDurTable[d1r[cha] + rcOffset[cha]];
                 envStepLevelIncClock[cha] = clock + envStepLevelDur[cha];
                 envStepLevelInc[cha] = 1;
-                if (reverb[cha]) {
-                    envStepNextAtLevel[cha] = 24;   // ~ 18 dB
-                    envStepNext[cha] = REVERB;
-                } else {
+                // if (reverb[cha]) {
+                //     envStepNextAtLevel[cha] = 24;   // ~ 18 dB
+                //     envStepNext[cha] = REVERB;
+                // } else {
                     envStepNextAtLevel[cha] = dl[cha] << 3;
                     envStepNext[cha] = DECAY2;
-                }
+                // }
                 break;
             case DECAY2:
-                envStepLevelDur[cha] = d2r[cha] === 0 ? 0 : rateDecayDurTable[d2r[cha] + rcOffset[cha]];
-                envStepLevelIncClock[cha] = clock + envStepLevelDur[cha];
-                envStepLevelInc[cha] = 1;
-                if (reverb[cha] && envLevel[cha] < 24) {
-                    envStepNextAtLevel[cha] = 24;   // ~ 18 dB
-                    envStepNext[cha] = REVERB;
-                } else {
+                // envStepLevelDur[cha] = d2r[cha] === 0 ? 0 : rateDecayDurTable[d2r[cha] + rcOffset[cha]];
+                // envStepLevelIncClock[cha] = clock + envStepLevelDur[cha];
+                // envStepLevelInc[cha] = 1;
+                // if (reverb[cha] && envLevel[cha] < 24) {
+                //     envStepNextAtLevel[cha] = 24;   // ~ 18 dB
+                //     envStepNext[cha] = REVERB;
+                // } else {
                     envStepNextAtLevel[cha] = 128;
                     envStepNext[cha] = IDLE;
-                }
+                // }
                 break;
             case RELEASE:
                 envStepLevelDur[cha] = rr[cha] === 0 ? 0 : rateDecayDurTable[rr[cha] + rcOffset[cha]];
