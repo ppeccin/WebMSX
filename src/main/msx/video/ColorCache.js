33b48f9358cf332f6c9adc815efb66a44d95a9c6
ppeccin <ppeccin@gmail.com>
2020-01-03 23:27:06 -0300

    v6.0 WIP: New palettes for MSX1, B&W and Green palettes

--- src/main/msx/video/ColorCache.js
+++ src/main/msx/video/ColorCache.js
@@ -17,7 +17,28 @@ wmsx.ColorCache = new function() {
     var color2to8bits9990 = [ 0, 90, 172, 255 ];                        // 8 bit B values for 2 bit B colors. Perfect Grays in 256 color mode NOT possible!
     var color3to8bits9990 = [ 0, 32, 74, 106, 148, 180, 222, 255 ];     // 8 bit R,G values for 3 bit R,G colors
 
-    var color5to8bits = [ 0, 8, 16, 24, 32, 41, 49, 57, 65, 74, 82, 90, 98, 106, 115, 123, 131, 139, 148, 156, 164, 172, 180, 189, 197, 205, 213, 222, 230, 238, 246, 255 ];    // 8 bit R,G,B values for 5 bit R,G,B colors
+    var color5to8bits =     [ 0, 8, 16, 24, 32, 41, 49, 57, 65, 74, 82, 90, 98, 106, 115, 123, 131, 139, 148, 156, 164, 172, 180, 189, 197, 205, 213, 222, 230, 238, 246, 255 ];    // 8 bit R,G,B values for 5 bit R,G,B colors
+
+
+    this.initialPalettesValues = [
+        // 0: WebMSX original 9918
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff28ca07, 0xff65e23d, 0xfff04444, 0xfff46d70, 0xff1330d0, 0xfff0e840, 0xff4242f3, 0xff7878f4, 0xff30cad0, 0xff89dcdc, 0xff20a906, 0xffc540da, 0xffbcbcbc, 0xffffffff ]),
+        // 1: 9918
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff0deb00, 0xff50f340, 0xffff474d, 0xffff6679, 0xff2b45f9, 0xffffff12, 0xff2d45ff, 0xff5069ff, 0xff05cbde, 0xff44d4f0, 0xff0bcb00, 0xffe246e4, 0xffcccccc, 0xffffffff ]),
+        // 2: 9928
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff4ab740, 0xff7ecf75, 0xffd75659, 0xffef7780, 0xff515fb8, 0xffeedb66, 0xff5a66d9, 0xff7e8afd, 0xff60c3cc, 0xff88d0de, 0xff42a13b, 0xffb467b6, 0xffcccccc, 0xffffffff ]),
+        // 3: 9938
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff24db24, 0xff6dff6d, 0xffff2424, 0xffff6d49, 0xff2424b6, 0xffffdb49, 0xff2424ff, 0xff6d6dff, 0xff24dbdb, 0xff92dbdb, 0xff249224, 0xffb649db, 0xffb6b6b6, 0xffffffff ]),
+        // 4: Toshiba VDP
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff66cc66, 0xff88ee88, 0xffdd4444, 0xffff7777, 0xff5555bb, 0xffdddd77, 0xff6666dd, 0xff7777ff, 0xff55cccc, 0xff88eeee, 0xff55aa55, 0xffbb55bb, 0xffcccccc, 0xffffffff ]),
+        // 5: Fujitsu FM-X
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff00ff00, 0xff00ff00, 0xffff0000, 0xffff0000, 0xff0000ff, 0xffffff00, 0xff0000ff, 0xff0000ff, 0xff00ffff, 0xff00ffff, 0xff00ff00, 0xffff00ff, 0xffffffff, 0xffffffff ]),
+
+        // 6: BW Toshiba VDP
+        new Uint32Array([ 0xff000000, 0xff000000, 0xffafafaf, 0xffd1d1d1, 0xff4f4f4f, 0xff848484, 0xff6b6b6b, 0xffc0c0c0, 0xff7f7f7f, 0xff949494, 0xffc6c6c6, 0xffe7e7e7, 0xff929292, 0xff727272, 0xffcccccc, 0xffffffff ]),
+        // 7: Green Toshiba VDP
+        new Uint32Array([ 0xff000000, 0xff000000, 0xff3aaf3a, 0xff45d145, 0xff1a4f1a, 0xff2c842c, 0xff236b23, 0xff40c040, 0xff2a7f2a, 0xff319431, 0xff42c642, 0xff4de74d, 0xff309230, 0xff267226, 0xff44cc44, 0xff55ff55 ])
+    ];
 
 
     this.getColors8bit9938Values = function() {
