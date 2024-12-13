a6ee49952558e9f9ba7406c6749392879174ea92
ppeccin <ppeccin@gmail.com>
2024-12-13 20:29:44 -0300

    Release 6.0.6

--- src/main/Launcher.js
+++ src/main/Launcher.js
@@ -117,7 +117,7 @@ if (window.applicationCache) {
     else window.applicationCache.addEventListener("updateready", onUpdateReady);
 }
 
-WMSX.VERSION = "6.0.5";
+WMSX.VERSION = "6.0.6";
 
 // Start pre-loading images right away
 WMSX.preLoadImagesAndStart();
